2d518c8cb886cf9f59febbcac659f825772999b7
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2022-09-07 18:30:24 -0300

    feat: add function to delete todo

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -11,8 +11,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors.blue,
-        secondary: colors.pink,
+        primary: colors.purple,
       },
     },
   },
--- lib/petal_web/live/todo_view.ex
+++ lib/petal_web/live/todo_view.ex
@@ -17,6 +17,12 @@ defmodule PetalWeb.TodoView do
     {:noreply, fetch(socket)}
   end
 
+  def handle_event("delete", %{"id" => id}, socket) do
+    todo = Todos.get_todo!(id)
+    Todos.delete_todo(todo)
+    {:noreply, fetch(socket)}
+  end
+
   def handle_event("toggle_done", %{"id" => id}, socket) do
     todo = Todos.get_todo!(id)
     Todos.update_todo(todo, %{done: !todo.done})
